2e5ab8920dfc0f9210705662044985cd074225d8
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-17 16:29:22 -0500

    Remove unused providers and rename remaining provider

--- providers.tf
+++ providers.tf
@@ -1,25 +1,7 @@
 provider "aws" {
   region = var.aws_region
   assume_role {
-    role_arn     = var.default_role_arn
+    role_arn     = var.terraform_role_arn
     session_name = "terraform-default"
-  }
-}
-
-provider "aws" {
-  alias  = "public_dns"
-  region = var.aws_region
-  assume_role {
-    role_arn     = var.dns_role_arn
-    session_name = "terraform-public-dns"
-  }
-}
-
-provider "aws" {
-  alias  = "cert_read_role"
-  region = var.aws_region
-  assume_role {
-    role_arn     = var.cert_role_arn
-    session_name = "terraform-cert"
   }
 }
