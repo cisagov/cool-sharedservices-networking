579e56bfb1c312c4d2d2315f51a98683f7f5b56a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-17 16:15:03 -0500

    Remove undefined/unnecesary outputs/variables

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,3 @@
-output "ipa_client_security_group_id" {
-  value       = module.ipa_master.client_security_group_id
-  description = "The ID corresponding to the IPA client security group"
-}
-
-output "ipa_master_cert_read_role_arn" {
-  value       = module.certreadrole_ipa_master.arn
-  description = "The ARN corresponding to the role used by the IPA master to read its certificate information"
-}
-
-output "ipa_master_id" {
-  value       = module.ipa_master.id
-  description = "The EC2 instance ID corresponding to the IPA master"
-}
-
-output "ipa_replica1_cert_read_role_arn" {
-  value       = module.certreadrole_ipa_replica1.arn
-  description = "The ARN corresponding to the role used by the first IPA replica to read its certificate information"
-}
-
-output "ipa_replica1_id" {
-  value       = module.ipa_replica1.id
-  description = "The EC2 instance ID corresponding to the first IPA replica"
-}
-
-output "ipa_replica2_cert_read_role_arn" {
-  value       = module.certreadrole_ipa_replica2.arn
-  description = "The ARN corresponding to the role used by the second IPA replica to read its certificate information"
-}
-
-output "ipa_replica2_id" {
-  value       = module.ipa_replica2.id
-  description = "The EC2 instance ID corresponding to the second IPA replica"
-}
-
-output "ipa_server_security_group_id" {
-  value       = module.ipa_master.server_security_group_id
-  description = "The ID corresponding to the IPA server security group"
-}
-
 output "private_subnet_ids" {
   value       = module.private_subnets.subnet_ids
   description = "The subnets IDs corresponding to the private subnets in the VPC"
@@ -46,11 +6,6 @@ output "private_subnet_ids" {
 output "private_subnet_nat_gw_ids" {
   value       = zipmap(module.private_subnets.subnet_ids, aws_nat_gateway.nat_gws[*].id)
   description = "The IDs corresponding to the NAT gateways used in the private subnets in the VPC"
-}
-
-output "private_zone_id" {
-  value       = aws_route53_zone.private_zone.zone_id
-  description = "The zone ID corresponding to the private Route53 zone for the VPC"
 }
 
 output "private_subnet_private_reverse_zone_ids" {
--- variables.tf
+++ variables.tf
@@ -4,36 +4,8 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "cert_role_arn" {
-  description = "The ARN of the role to assume when creating a role to allow reading certboto certificate data"
-}
-
 variable "default_role_arn" {
   description = "The ARN of the role to assume when performing most Terraform tasks"
-}
-
-variable "dns_role_arn" {
-  description = "The ARN of the role to assume when performing public DNS Terraform tasks"
-}
-
-variable "ipa_admin_pw" {
-  description = "The password for the Kerberos admin role"
-}
-
-variable "ipa_directory_service_pw" {
-  description = "The password for the IPA master's directory service"
-}
-
-variable "ipa_master_cert_pw" {
-  description = "The password for the IPA master's certificate"
-}
-
-variable "ipa_replica1_cert_pw" {
-  description = "The password for the first IPA replica's certificate"
-}
-
-variable "ipa_replica2_cert_pw" {
-  description = "The password for the second IPA replica's certificate"
 }
 
 variable "private_subnet_cidr_blocks" {
@@ -59,21 +31,6 @@ variable "vpc_cidr_block" {
 variable "aws_region" {
   description = "The AWS region where the shared services account is to be created (e.g. \"us-east-1\")."
   default     = "us-east-1"
-}
-
-variable "cert_bucket_name" {
-  description = "The AWS region where the shared services account is to be created (e.g. \"us-east-1\")."
-  default     = "cool-certificates"
-}
-
-variable "cool_domain" {
-  description = "The domain where the COOL resources reside (e.g. \"cool.cyber.dhs.gov\")."
-  default     = "cool.cyber.dhs.gov"
-}
-
-variable "public_zone_name" {
-  description = "The name of the public Route53 zone in Route53 where public DNS records should be created (e.g. \"cyber.dhs.gov.\")."
-  default     = "cyber.dhs.gov."
 }
 
 variable "tags" {
