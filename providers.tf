22b0adac3544226f505aab9861c717f5eba49a3e
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-23 16:01:05 -0500

    Add a provider for the cool-master-organizationsreadonly role

--- providers.tf
+++ providers.tf
@@ -2,3 +2,9 @@ provider "aws" {
   profile = "cool-sharedservices-provisionaccount"
   region  = var.aws_region
 }
+
+provider "aws" {
+  alias   = "organizationsreadonly"
+  profile = "cool-master-organizationsreadonly"
+  region  = var.aws_region
+}
