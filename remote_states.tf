7b4121e1ec73a204e7369037683ee9f4cf40c460
Dave Redmin <david.redmin@beta.dhs.gov>
2020-07-09 15:03:19 -0400

    Add Terraform remote states for Master and Shared Services accounts.

---
+++ remote_states.tf
@@ -0,0 +1,35 @@
+# ------------------------------------------------------------------------------
+# Retrieves state data from a Terraform backend. This allows use of the
+# root-level outputs of one or more Terraform configurations as input data
+# for this configuration.
+# ------------------------------------------------------------------------------
+
+data "terraform_remote_state" "master" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/master.tfstate"
+  }
+
+  workspace = "production"
+}
+
+data "terraform_remote_state" "sharedservices" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/shared_services.tfstate"
+  }
+
+  workspace = terraform.workspace
+}
