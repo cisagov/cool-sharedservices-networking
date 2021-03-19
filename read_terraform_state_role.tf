b23fe7beb98cf88bc26230f73cce0dc8a9535ae3
David Redmin <david.redmin@trio.dhs.gov>
2021-03-18 22:15:23 -0400

    Use terraform-state-read-role-tf-module to create IAM policies and role

--- read_terraform_state_role.tf
+++ read_terraform_state_role.tf
@@ -1,21 +1,20 @@
 # ------------------------------------------------------------------------------
-# Create the IAM role that allows read-only access to the
-# cool-sharedservices-networking state in the Terraform state bucket.
+# Create the IAM policy and role that allows read-only access to the Terraform
+# state for this project in the S3 bucket where Terraform remote state is
+# stored.
 # ------------------------------------------------------------------------------
 
-resource "aws_iam_role" "read_terraform_state" {
-  provider = aws.terraformprovisionaccount
+module "read_terraform_state" {
+  source = "github.com/cisagov/terraform-state-read-role-tf-module"
 
-  assume_role_policy = data.aws_iam_policy_document.assume_role_doc.json
-  description        = var.read_terraform_state_role_description
-  name               = var.read_terraform_state_role_name
-  tags               = var.tags
-}
+  providers = {
+    aws       = aws.terraformprovisionaccount
+    aws.users = aws.users
+  }
 
-# Attach the IAM policy to the role
-resource "aws_iam_role_policy_attachment" "read_terraform_state" {
-  provider = aws.terraformprovisionaccount
-
-  policy_arn = aws_iam_policy.read_terraform_state.arn
-  role       = aws_iam_role.read_terraform_state.name
+  account_ids                 = [local.users_account_id]
+  role_name                   = var.read_terraform_state_role_name
+  role_tags                   = var.tags
+  terraform_state_bucket_name = "cisa-cool-terraform-state"
+  terraform_state_path        = "cool-sharedservices-networking/*.tfstate"
 }
