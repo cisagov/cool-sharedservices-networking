d5772213dbd26f566564985232b946d00949cdeb
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-03-30 13:12:13 -0400

    Remove the workspace tag from the TF state read role
    It makes no sense to associate such a tag with the Terraform read sole, since it can read the state from any workspace.
    
    Such a tag will also flip flop as one switches from staging to production or vice versa, which is highly annoying.

--- read_terraform_state_role.tf
+++ read_terraform_state_role.tf
@@ -12,9 +12,15 @@ module "read_terraform_state" {
     aws.users = aws.users
   }
 
-  account_ids                 = [local.users_account_id]
-  role_name                   = var.read_terraform_state_role_name
-  role_tags                   = var.tags
+  account_ids = [local.users_account_id]
+  role_name   = var.read_terraform_state_role_name
+  # It makes no sense to associate a "Workspace" tag with the
+  # Terraform read role, since it can read the state from any
+  # workspace.
+  #
+  # Such a tag will also flip flop as one switched from staging to
+  # production or vice versa, which is highly annoying.
+  role_tags                   = { for k, v in var.tags : k => v if k != "Workspace" }
   terraform_state_bucket_name = "cisa-cool-terraform-state"
   terraform_state_path        = "cool-sharedservices-networking/*.tfstate"
 }
