46f39fc610b559c97b0a216d07ad38ccceec2791
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-05-17 19:41:38 -0400

    Add AWS S3 Gateway endpoint
    Add a vpc_endpoints.tf file to house any endpoints for this project. Add an S3
    gateway endpoint for this project in the aforementioned file.

---
+++ vpc_endpoints.tf
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Create the VPC endpoints.
+#-------------------------------------------------------------------------------
+
+#
+# VPC gateway endpoints
+#
+
+# S3 gateway endpoint
+resource "aws_vpc_endpoint" "s3" {
+  provider = aws.sharedservicesprovisionaccount
+
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  vpc_id            = aws_vpc.the_vpc.id
+  tags              = var.tags
+}
